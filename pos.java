5f9e3efb444ed383b6671465582d23d5bfec795f
priyankaprema <30485652+priyankaprema@users.noreply.github.com>
2017-07-28 21:30:22 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class pos
+{
+public static void main(String s[])
+{
+Scanner sc=new Scanner(System.in);
+int a;
+if(a>0)
+{
+System.out.println("positive number");
+}
+else if(a==0)
+{
+System.out.println("number is zero");
+}
+else
+{
+System.out.println("negative number");
+}
+}
+}
