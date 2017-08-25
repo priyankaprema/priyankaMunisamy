c912cd90cdccae07d30fd71ba18e71ed258875f5
priyankaprema <30485652+priyankaprema@users.noreply.github.com>
2017-08-25 20:50:10 +0530

    Create into.java

---
+++ into.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class into
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+a=sc.nextInt();
+String s=Integer.toString(a)
+System.out.println(s);
+}
+}
