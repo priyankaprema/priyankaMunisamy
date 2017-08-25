ca79f8d76d4286e6d00951a4cdf8d0f8d410df56
priyankaprema <30485652+priyankaprema@users.noreply.github.com>
2017-08-25 20:15:21 +0530

    Create factt.java

---
+++ factt.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class factt
+{
+public static void main(String args[])
+{
+int n,a=0,b=1,c;
+Scanner sc=new Scanner(System.in);
+Sytem.out.println("enter the number of series");
+n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+System.out.println(a);
+c=a+b;
+a=b;
+b=c;
+}
+}
+}
