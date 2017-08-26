9d1a51b3221023bc45032db3a5e4ac41d6151a05
priyankaprema <30485652+priyankaprema@users.noreply.github.com>
2017-08-26 09:41:35 +0530

    Create work.java

---
+++ work.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class work
+{
+public static void main(String args[])
+{
+String str;
+Scanner sc=new Scanner(System.in);
+str=sc.next();
+if(str.equals("sunday")||str.equals("SUNDAY"))
+{
+System.out.println("false");
+}
+else
+{
+System.out.println("true");
+}
+}
+}
