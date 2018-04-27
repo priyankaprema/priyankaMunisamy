b55abbd28634d2f952d52a6af7abaa0a373234e0
priyankaprema <30485652+priyankaprema@users.noreply.github.com>
2018-04-27 20:55:10 +0530

    Create a.java

---
+++ a.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class a
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c;
+c=a+b;
+System.out.println(c);
+}
+}
