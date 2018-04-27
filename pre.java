a83325461f27ca5dbef8d4cfd2980787a085ff87
priyankaprema <30485652+priyankaprema@users.noreply.github.com>
2018-04-27 20:41:56 +0530

    Create pre.java

---
+++ pre.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+class pre
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s,s1;
+s=sc.next();
+s1=sc.next();
+int i=0,k=0,j=0,m,n;
+m=s.length();
+n=s1.length();
+if((m>=1&&m<=100000)&&(n>=1&&n<=100000))
+{
+for(i=0;i<m;i++)
+{    k=0;
+for(j=i;j<=i;j++)
+{
+  if(s.charAt(i)==s1.charAt(j))
+  {
+  k=1;
+  System.out.print(s.charAt(i));
+  }
+}
+if(k==0)
+{
+break;
+}
+}
+}
+}
+}
